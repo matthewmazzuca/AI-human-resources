d480eef5f8d6d67e64481e1ae7801d62b9443b0a
matthewmazzuca <matthew.mazzuca@mail.utoronto.ca>
2016-10-21 13:15:56 -0400

    final

--- src/demo.js
+++ src/demo.js
@@ -12,6 +12,8 @@ angular.module('app', ['ngMaterial', 'mwFormBuilder', 'mwFormViewer', 'mwFormUti
         $scope.timeforAI = false
         $scope.processing = false
         $scope.submitted = "Hello world"
+        $scope.finalPage = false
+        ctrl.textProgress = "Please wait while we gather data from current bank employees"
         ctrl.cmergeFormWithResponse = false;
         ctrl.cgetQuestionWithResponseList = false;
         ctrl.cgetResponseSheetHeaders = false;
@@ -25,6 +27,23 @@ angular.module('app', ['ngMaterial', 'mwFormBuilder', 'mwFormViewer', 'mwFormUti
         $http.get('form-data.json')
             .then(function(res){
                 ctrl.formData = res.data;
+            });
+        ctrl.usersData = null;
+        ctrl.usersFinal = null
+        $http.get('users.json')
+            .then(function(res){
+                ctrl.usersData = res.data;
+                // console.log(ctrl.usersData)
+                console.log(ctrl.usersData.length)
+                console.log(Math.random())
+                var rand = ctrl.usersData[Math.floor(Math.random() * ctrl.usersData.length)];
+                var rand2 = ctrl.usersData[Math.floor(Math.random() * ctrl.usersData.length)];
+                var rand3 = ctrl.usersData[Math.floor(Math.random() * ctrl.usersData.length)];
+                rand.tweet = "Just wanted to share this moment.  The first time Darryl saw his future wife.  Congratulations @DarrylSmart1 on your nuptials."
+                rand2.tweet = "Your Pro-Fit Chiefs would like to wish a very happy birthday to our very own #71 Brier Jonathan "
+                rand3.tweet = "Throwing it back to my first Mann cup"
+                ctrl.usersFinal = [rand, rand2, rand3]
+                console.log(ctrl.usersFinal)
             });
         ctrl.formBuilder={};
         ctrl.formViewer = {};
@@ -63,8 +82,13 @@ angular.module('app', ['ngMaterial', 'mwFormBuilder', 'mwFormViewer', 'mwFormUti
                 $scope.processing = false
                 angular.element("#play-pause-button").trigger( "click" );
                 $timeout( function(){ 
+                    ctrl.textProgress = "Please wait while we find the best Aboriginal candidates"
                     $scope.timeforAI = false
                     $scope.processing = true
+                    $timeout( function(){
+                        $scope.processing = false
+                        $scope.finalPage = true    
+                    }, 5000);
                     // angular.element("#play-pause-button"    ).trigger( "click" );
                  }, 20000);
 
